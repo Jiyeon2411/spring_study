0d29eced5f24092a96ba5e70cf28ddcf65e45058
jiyeon2411 <daz128@naver.com>
2023-06-19 17:38:51 +0900

    spring

--- board2/src/main/java/com/example/board2/dao/BoardDao.java
+++ board2/src/main/java/com/example/board2/dao/BoardDao.java
@@ -1,8 +1,19 @@
 package com.example.board2.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.board2.dto.Board;
 
 @Mapper
 public interface BoardDao {
-
+	public int maxNum() throws Exception;
+	
+	public void insertData(Board board) throws Exception; //dao는 항상 추상메소드만
+	
+	public int getDataCount(String searchKey, String searchValue) throws Exception;
+	
+	public List<Board> getLists(String searchKey, String searchValue, 
+			int start, int end) throws Exception;
 }
--- board2/src/main/java/com/example/board2/service/BoardService.java
+++ board2/src/main/java/com/example/board2/service/BoardService.java
@@ -1,5 +1,16 @@
 package com.example.board2.service;
 
-public interface BoardService {
+import java.util.List;
 
-}
+import com.example.board2.dto.Board;
+
+public interface BoardService {
+		public int maxNum() throws Exception;
+		
+		public void insertData(Board board) throws Exception; //dao는 항상 추상메소드만
+		
+		public int getDataCount(String searchKey, String searchValue) throws Exception;
+		
+		public List<Board> getLists(String searchKey, String searchValue, 
+				int start, int end) throws Exception;
+	}
