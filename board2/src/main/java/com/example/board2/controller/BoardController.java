56d9b4934c65d4fa68f6a8a2c9960abe1be107f9
jiyeon2411 <daz128@naver.com>
2023-06-20 17:12:05 +0900

    spring

--- board2/src/main/java/com/example/board2/controller/BoardController.java
+++ board2/src/main/java/com/example/board2/controller/BoardController.java
@@ -169,6 +169,7 @@ public class BoardController {
 			
 		} catch (Exception e) {
 			e.printStackTrace();
+			model.addAttribute("errorMessage", "게시글을 불러오는 중 에러가 발생하였습니다.");
 		}
 		
 		return "bbs/article";
